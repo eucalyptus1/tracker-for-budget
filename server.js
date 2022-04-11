3b598f7600689e66706be0e9d85a6521cc1dac3a
euc1 <jamesharrington585@hotmail.com>
2022-04-10 20:06:50 -0400

    testing heroku

--- server.js
+++ server.js
@@ -16,10 +16,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(MONGODB_URI, 'mongodb://localhost:3001', {
   useNewUrlParser: true,
   useFindAndModify: false
 });
+
+// app.get('/index', (req, res) => {
+//   res.sendFile(path.join(__dirname, './public/index.html'));
+// });
 
 // routes
 app.use(require("./routes/api.js"));
